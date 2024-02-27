721ad7f74287db3f66b8db64fad073e62b60b29a
joandercim <joacim@josiasmusik.se>
2024-02-27 20:41:33 +0100

    MongoDB connection etc

--- server.js
+++ server.js
@@ -1,22 +1,41 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Route files
-const bootcamps = require('./routes/bootcamps')
+const morgan = require('morgan')
+const connectDB = require('./config/db')
+const colors = require('colors')
 
 // Load env variables:
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
 
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
 // Mount routers
-app.use('/api/v1/bootcamps', bootcamps)
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT;
 
-
-
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold)
 );
+
+// Handle unhandeled rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+
+  // Close server and exit process
+  server.close(() => {
+    process.exit(1);
+  });
+})

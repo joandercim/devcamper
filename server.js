e35819802d759d07ed4ddf5bb53036bf2ccab69d
Joacim Andersson <joacim@josiasmusik.se>
2024-02-27 16:10:58 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Route files
+const bootcamps = require('./routes/bootcamps')
+
+// Load env variables:
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps)
+
+const PORT = process.env.PORT;
+
+
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
